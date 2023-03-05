c995e005135a94ad19fcfff75b992c7b57c3e98e
Igor D <logerr23@gmail.com>
2023-03-05 18:23:36 +0300

    test passed

--- src/test/java/PostTest.java
+++ src/test/java/PostTest.java
@@ -19,7 +19,7 @@ public class PostTest {
         // Проверки
         .then()
             .statusCode(200)
-            .body("data", equalTo("some"))
+            .body("data", equalTo("some data"))
         ;
 
     }
